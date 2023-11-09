8f396beb62850c1ff66c8b2371b75899fbefaf3b
Giovanna Vieira <126898074+giivieira@users.noreply.github.com>
2023-11-09 11:24:09 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,32 +1,25 @@
 from urllib import request
-
 from fastapi import FastAPI, HTTPException, status, Response, Path
-from models import Dog
 import requests
 
 app = FastAPI()
 
 roupas = {
-    {"id": 1,
-     "nome:": "Camiseta",
-     "categoria": "Rouopas Masculinas"},
-    {"id": 2,
-     "nome:": "Vestido",
-     "categoria": "Rouopas Femininas"}
+    "1": {
+        "id": 1,
+        "nome:": "Camiseta",
+        "categoria": "Rouopas Masculinas"
+    },
+    "2": {
+        "id": 2,
+        "nome:": "Vestido",
+        "categoria": "Rouopas Femininas"
+    }
 }
 
 @app.get("/roupas")
 async def get_roupas():
     return {"Message": [request.json(), roupas]}
-
-
-#Rota para listar todas as roupas
-@app.route
-
-# Endpoint para listar roupas
-@app.route('/roupas', methods=['GET'])
-def listar_roupas():
-    #Consumir a API aqui usando a biblioteca 'requests' e retornar os dados no formato JSON
 
 #Endpoint para criar uma nova roupa
 @app.route('/roupas', methods=['POST'])
@@ -37,18 +30,19 @@ def criar_roupa():
     # Enviar a nova roupa para a API de roupas usando requests
 
 #End point para atualizar uma roupa existente
-@app.route('/roupas/<int;id_roupa>',
-           methods=['PUT'])
+@app.route('/roupas/<int;id_roupa>', methods=['PUT'])
 def atualizar_roupa(id_roupa):
     #Receber os dados atualizados da roupa do corpo da solicitação (request)
     dados_atualizados = request.json
     # Atualizar a roupa com o ID fornecido na API de roupas
-    # Retornar uma resposta adequada
+    # Retornar uma resposta adequada 
 
     #Endpoint para excluir uma roupa
 @app.route('/roupas/<int:id_roupa>', methods=['DELETE'])
 def excluir_roupa(id_roupa):
 # Excluir a roupa com o ID fornecido na API
+    ...
+
 
 if __name__ == '__main':
     app.run(debug=True)
